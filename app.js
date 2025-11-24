cd605bffe0faab69e2260a499d338659e04d08fa
akthernahela <nahela00@gmail.com>
2025-11-24 16:50:13 +0100

    Add the express doc example to have an idea to complete the process

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express')
 const app = express()
-const POST = 3000
+const PORT = 3000
 
 app.use(express.static('pubblic'));
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
